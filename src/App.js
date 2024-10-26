e887eb588af8c537cb1d44ec8830319fd9dc7066
mhmtkarakas <m.karakas32@gmail.com>
2024-10-26 21:14:35 +0300

    navbar editted

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.css";
 import NavbarComp from "./components/Navbar/NavbarComp";
+import AOS from 'aos';
+import 'aos/dist/aos.css'; // Import AOS styles
+import React, { useEffect } from 'react';
 
 function App() {
+  useEffect(() => {
+    AOS.init({
+      duration: 1000,   // Animation duration (1 second)
+    });
+  }, []); // Empty dependency array to ensure it runs once on component mount
+
   return (
     <div>
       <BrowserRouter>
--- src/components/Navbar/NavbarComp.jsx
+++ src/components/Navbar/NavbarComp.jsx
@@ -1,62 +1,45 @@
 import React from "react";
 import "./NavbarComp.css";
-import Container from "react-bootstrap/Container";
-import Nav from "react-bootstrap/Nav";
-import Navbar from "react-bootstrap/Navbar";
+import { Container, Nav, Navbar } from "react-bootstrap/";
 import { Link } from "react-router-dom";
-import { FaFacebook } from "react-icons/fa";
-import { FaInstagram } from "react-icons/fa";
-import { FaLinkedin } from "react-icons/fa";
+import { FaFacebook, FaInstagram, FaLinkedin } from "react-icons/fa";
 
 const NavbarComp = () => {
   return (
     <div className="main-container">
       <Navbar expand="lg" className="fixed-top ">
-        <Container className="navClass">
+        <Container fluid className="navClass">
           <Navbar.Brand>
             <Link to="/" className="navbar-brand text-dark fw-bold">
-              ARYA MIMARLIK
+              ARYA MİMARLIK
             </Link>
           </Navbar.Brand>
+
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
-            <Nav className=" justify-content-end w-100 ">
-              <Nav.Link
-                href="/"
-                className="active text-uppercase d-flex align-items-center mx-3  "
-              >
+            <Nav className="justify-content-end  w-100 ">
+              <Nav.Link href="/" className="active text-uppercase mx-2 ">
                 {" "}
                 <span>Anasayfa</span>
               </Nav.Link>
 
-              <Nav.Link
-                href="/about"
-                className="text-uppercase d-flex align-items-center mx-2 "
-              >
-               <span>KURUMSAL</span> {" "}
+              <Nav.Link href="/about" className="text-uppercase mx-2 ">
+                <span>KURUMSAL</span>{" "}
               </Nav.Link>
-              <Nav.Link
-                href="/about"
-                className="text-uppercase d-flex align-items-center mx-2 "
-              >
-               <span>IS ORTAKLARIMIZ</span> {" "}
+              <Nav.Link href="/about" className="text-uppercase mx-2 ">
+                <span>İŞ ORTAKLARIMIZ</span>{" "}
               </Nav.Link>
-              <Nav.Link
-                href="/about"
-                className="text-uppercase d-flex align-items-center mx-2 "
-              >
-               <span>REFERANSLARIMIZ</span>{" "}
+              <Nav.Link href="/about" className="text-uppercase  mx-2 ">
+                <span>REFERANSLARIMIZ</span>{" "}
               </Nav.Link>
-              <Nav.Link
-                href="/contact"
-                className="text-uppercase d-flex align-items-center mx-2 "
-              >
-               <span>Iletisim</span> 
+              <Nav.Link href="/contact" className="text-uppercase mx-2 ">
+                <span>İletİŞİm</span>
               </Nav.Link>
+
               <div className="navIcons">
-              <FaFacebook />
-              <FaInstagram />
-              <FaLinkedin />
+                <FaFacebook />
+                <FaInstagram />
+                <FaLinkedin />
               </div>
             </Nav>
           </Navbar.Collapse>
